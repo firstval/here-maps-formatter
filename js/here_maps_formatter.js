83f2e0fa77ce64acb8993c9725341738702951c7
ranelpadon <ranel.padon@gmail.com>
2014-12-28 18:11:46 +0800

    Add other zoom level values

--- js/here_maps_formatter.js
+++ js/here_maps_formatter.js
@@ -16,7 +16,8 @@
     var latitude = mapFeatures.lat;
     var longitude = mapFeatures.lon;
 
-    // Initialize the platform object:
+    // Initialize the platform object.
+    // @todo Create an Drupal Admin UI for inputting this credentials.
     var platform = new H.service.Platform({
       'app_id': 'DemoAppId01082013GAL',
       'app_code': 'AJKnXv84fjrb0KIHawS0Tg',

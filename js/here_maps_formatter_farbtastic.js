785e96070da0a3fc105e0dabdfbff6c4d0b3a204
ranelpadon <ranel.padon@gmail.com>
2015-01-26 20:20:32 +0800

    Fix Farbtastic referencing issue when the target element is not rendered yet

--- js/here_maps_formatter_farbtastic.js
+++ js/here_maps_formatter_farbtastic.js
@@ -6,16 +6,22 @@
 (function ($) {
   Drupal.behaviors.here_maps_formatter_farbtastic = {
     attach : function(context, settings) {
-      $(context).ajaxComplete(function() {
-        // Create a farbtastic colorpicker in the given DIV container.
-        var colorPicker = $.farbtastic('#here-maps-formatter-color-picker');
+      // Wait the HEX input field to be rendered completely to prevent
+      // referencing errors. Pass the context information to speed up
+      // DOM traversal/searching.
+      $('#here-maps-formatter-color-picker-target', context).ajaxComplete(function() {
+        // Prevent multiple processing.
+        $('#here-maps-formatter-color-picker-target', context).once('color-picker-target-processed', function() {
+          // Create a farbtastic colorpicker in the given DIV container.
+          var colorPicker = $.farbtastic('#here-maps-formatter-color-picker');
 
-        // Retrieve the target text field that contains the HEX color strings.
-        var colorPickerTarget = $('#here-maps-formatter-color-picker-target');
+          // Retrieve the target text field that contains the HEX color strings.
+          var colorPickerTarget = $('#here-maps-formatter-color-picker-target');
 
-        // Make the color picker auto-update the contents of the HEX text field.
-        colorPicker.linkTo(colorPickerTarget);
+          // Make the color picker auto-update the contents of the HEX text field.
+          colorPicker.linkTo(colorPickerTarget);
+        });
       });
-    }
+    },
   };
 })(jQuery);
